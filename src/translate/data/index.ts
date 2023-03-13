c9d0105583aa887743c30e8f4f5fe1b6627658da
kumadayo <101532796+kuma-dayo@users.noreply.github.com>
2023-03-13 22:00:55 +0900

    add(translate): Japanese Language Support
    Since most of the translation was done mechanically
    some corrections are necessary.

--- src/translate/data/index.ts
+++ src/translate/data/index.ts
@@ -1,11 +1,13 @@
 import enUS from "./en-us.json"
 import zhCN from "./zh-cn.json"
 import zhTW from "./zh-tw.json"
+import jaJP from "./ja-jp.json"
 
 export const DEFAULT_LANG = "en-us"
 
 const LanguageData = {
   "en-us": enUS,
+  "ja-jp": jaJP,
   "zh-cn": zhCN,
   "zh-tw": zhTW,
 }
